267fde4484aa1ef34e01d251b11ed4d04bb0a9f1
jicjicjicjkjkjk <jkayse@hotmail.com>
2025-11-29 13:29:31 -0600

    Create players.js

---
+++ src/players.js
@@ -0,0 +1,15 @@
+// Players in alpha order by FIRST name
+export const PLAYERS = [
+  { id: "alanna",  number: 25, firstName: "Alanna",  lastName: "Tanny",    photoUrl: "" },
+  { id: "bella",   number: 12, firstName: "Bella",   lastName: "Lipana",   photoUrl: "" },
+  { id: "bridget", number: 28, firstName: "Bridget", lastName: "Murtagh",  photoUrl: "" },
+  { id: "brooke",  number: 13, firstName: "Brooke",  lastName: "Amelio",   photoUrl: "" },
+  { id: "elena",   number: 52, firstName: "Elena",   lastName: "Konrad",   photoUrl: "" },
+  { id: "emily",   number: 2,  firstName: "Emily",   lastName: "Johnsen",  photoUrl: "" },
+  { id: "josie",   number: 7,  firstName: "Josie",   lastName: "Kayse",    photoUrl: "" },
+  { id: "laney",   number: 9,  firstName: "Laney",   lastName: "O'Donnell",photoUrl: "" },
+  { id: "lilly",   number: 14, firstName: "Lilly",   lastName: "Coughlin", photoUrl: "" },
+  { id: "lucie",   number: 22, firstName: "Lucie",   lastName: "Weaver",   photoUrl: "" },
+  { id: "maggie",  number: 0,  firstName: "Maggie",  lastName: "Galloro",  photoUrl: "" },
+  { id: "molly",   number: 15, firstName: "Molly",   lastName: "Kunz",     photoUrl: "" }
+];
