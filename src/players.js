47b7213cf685f075603861b95f2cf9ea15d0f0fb
jicjicjicjkjkjk <jkayse@hotmail.com>
2025-11-29 18:06:29 -0600

    Update players.js

--- src/players.js
+++ src/players.js
@@ -1,15 +1,86 @@
-// Players in alpha order by FIRST name
 export const PLAYERS = [
-  { id: "alanna",  number: 25, firstName: "Alanna",  lastName: "Tanny",    photoUrl: "" },
-  { id: "bella",   number: 12, firstName: "Bella",   lastName: "Lipana",   photoUrl: "" },
-  { id: "bridget", number: 28, firstName: "Bridget", lastName: "Murtagh",  photoUrl: "" },
-  { id: "brooke",  number: 13, firstName: "Brooke",  lastName: "Amelio",   photoUrl: "" },
-  { id: "elena",   number: 52, firstName: "Elena",   lastName: "Konrad",   photoUrl: "" },
-  { id: "emily",   number: 2,  firstName: "Emily",   lastName: "Johnsen",  photoUrl: "" },
-  { id: "josie",   number: 7,  firstName: "Josie",   lastName: "Kayse",    photoUrl: "" },
-  { id: "laney",   number: 9,  firstName: "Laney",   lastName: "O'Donnell",photoUrl: "" },
-  { id: "lilly",   number: 14, firstName: "Lilly",   lastName: "Coughlin", photoUrl: "" },
-  { id: "lucie",   number: 22, firstName: "Lucie",   lastName: "Weaver",   photoUrl: "" },
-  { id: "maggie",  number: 0,  firstName: "Maggie",  lastName: "Galloro",  photoUrl: "" },
-  { id: "molly",   number: 15, firstName: "Molly",   lastName: "Kunz",     photoUrl: "" }
+  {
+    id: "lucie",
+    number: 22,
+    firstName: "Lucie",
+    lastName: "Weaver",
+    pin: "2394", // last 4 of 18477362394
+  },
+  {
+    id: "josie",
+    number: 7,
+    firstName: "Josie",
+    lastName: "Kayse",
+    pin: "7332", // last 4 of 16307817332
+  },
+  {
+    id: "brooke",
+    number: 13,
+    firstName: "Brooke",
+    lastName: "Amelio",
+    pin: "0008", // last 4 of 18478330008
+  },
+  {
+    id: "lilly",
+    number: 14,
+    firstName: "Lilly",
+    lastName: "Coughlin",
+    pin: "0595", // last 4 of 16026790595
+  },
+  {
+    id: "maggie",
+    number: 0,
+    firstName: "Maggie",
+    lastName: "Galloro",
+    pin: "9592", // last 4 of 17737179592
+  },
+  {
+    id: "laney",
+    number: 9,
+    firstName: "Laney",
+    lastName: "O'Donnell",
+    pin: "4860", // last 4 of 13123714860
+  },
+  {
+    id: "alanna",
+    number: 25,
+    firstName: "Alanna",
+    lastName: "Tanny",
+    pin: "7921", // last 4 of 17734437921
+  },
+  {
+    id: "bridget",
+    number: 28,
+    firstName: "Bridget",
+    lastName: "Murtagh",
+    pin: "3564", // last 4 of 18473123564
+  },
+  {
+    id: "emily",
+    number: 2,
+    firstName: "Emily",
+    lastName: "Johnsen",
+    pin: "9918", // last 4 of 18476879918
+  },
+  {
+    id: "molly",
+    number: 15,
+    firstName: "Molly",
+    lastName: "Kunz",
+    pin: "2105", // last 4 of 17735622105
+  },
+  {
+    id: "bella",
+    number: 12,
+    firstName: "Bella",
+    lastName: "Lipana",
+    pin: "2183", // last 4 of 13124782183
+  },
+  {
+    id: "elena",
+    number: 52,
+    firstName: "Elena",
+    lastName: "Konrad",
+    pin: "6207", // last 4 of 16307766207
+  },
 ];
